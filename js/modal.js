f299b09fe9d447b9a947fd8ff28f5cf7cab469e2
MarynaPyvovar <marina18renkas@gmail.com>
2022-08-18 18:52:41 +0300

    feat sections css

--- js/modal.js
+++ js/modal.js
@@ -1,34 +1,32 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModalBtn.addEventListener("click", onOpenModal);
-  refs.closeModalBtn.addEventListener("click", onCloseModal);
-  refs.modal.addEventListener("click", onBackdropClick);
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.modal.addEventListener('click', onBackdropClick);
 
-  function onOpenModal() {
-    window.addEventListener('keydown', onKeyPress);
-    refs.modal.classList.toggle("is-hidden");
+function onOpenModal() {
+  window.addEventListener('keydown', onKeyPress);
+  refs.modal.classList.toggle('is-hidden');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onKeyPress);
+  refs.modal.classList.toggle('is-hidden');
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
   }
+}
 
-  function onCloseModal() {
-    window.removeEventListener('keydown', onKeyPress);
-    refs.modal.classList.toggle("is-hidden");
+function onKeyPress(event) {
+  const ESC_CODE = 'Escape';
+  if (event.code === ESC_CODE) {
+    onCloseModal();
   }
-
-  function onBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-      onCloseModal();
-    }
-  }
-
-  function onKeyPress(event) {
-    const ESC_CODE = 'Escape';
-    if (event.code === ESC_CODE) {
-      onCloseModal();
-    }
-  }
-})();
+}
